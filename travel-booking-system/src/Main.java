46a627528bd62ec9824d1938cc630b3fe6bfa7a9
GiorgiGogatishvili <147611477+GiorgiGogatishvili@users.noreply.github.com>
2026-03-10 17:04:49 +0400

    Update Main.java

--- travel-booking-system/src/Main.java
+++ travel-booking-system/src/Main.java
@@ -2,11 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        customer customer = new customer("Giorgi", "gogatishviligio@mail.com");
+        customer customer = new customer("Giorgi", "giorgi@mail.com");
 
         flight flight = new flight("Rome", 5);
+        taxibooking taxi = new taxibooking("Airport", "Hotel");
 
-        booking.bookFlight(customer, flight);
+       booking.bookFlight(customer, flight);
+
+        System.out.println("Taxi booked from "
+                + taxi.getPickupLocation()
+                + " to "
+                + taxi.getDestination());
 
         System.out.println("Total bookings: " +
                 booking.getTotalBookings());
